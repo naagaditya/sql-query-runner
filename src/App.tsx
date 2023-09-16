9322a5e6e48c31cca8328dabdc447821aa8917fc
adityanaag91 <aditya.naag@traveloka.com>
2023-09-16 14:00:24 +0530

    basic select query

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Users } from './dummyData/dummyTables';
+import Table from './table/Table';
 
 function App() {
+  const [filteredTable, setFilteredTable] = useState<Record<string, any>[]>([]);
+  const [query, setQuery] = useState('');
+
+  const AllTable: Record<string, Record<string, any>[]>  = {
+    Users 
+  };
+  
+  const handleChangeQuery = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const updatedQuery = e.target.value;
+    setQuery(updatedQuery);
+  }
+  const handleExecuteQuery = () => {
+    let columns: string = '';
+    let tableName: string = '';
+    [, columns, tableName] = query.match(/SELECT (.*) FROM (.*);/) || [];
+    let res;
+    if (columns && tableName) {
+      if (columns.trim() === '*') {
+        res = AllTable[tableName]
+      } else {
+        res = AllTable[tableName].map(row => 
+          columns.split(',').reduce((acc, col) => ({...acc, [`${col.trim()}`]: row[col.trim()] }), {}))
+      }
+      setFilteredTable(res);
+    } else {
+      console.error('Wrong query'); 
+    }
+    
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="query-input">
+        <textarea className="query-input" onChange={handleChangeQuery}></textarea>
+        <button onClick={handleExecuteQuery}>Run</button>
+      </div>
+      <div className="query-output">
+        <Table tableData={filteredTable}/>
+      </div>
+      <div className="avaiable-tables">
+        <Table tableData={Users}/>
+      </div>
     </div>
   );
 }
---
+++ src/table/Table.tsx
@@ -0,0 +1,35 @@
+import './Table.css';
+
+type Props = {
+  tableData: Record<string, any>[];
+}
+
+export default function Table({tableData}: Props) {
+  if (!tableData.length) return null;
+  return (
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(tableData[0]).map((data, i) => (
+            <HeaderCell key={`${data}${i}`} text={data}/>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((row, i) => <TableRow key={i} row={row} index={i}/>)}
+      </tbody>
+    </table>
+  )
+};
+
+const TableRow = ({row, index}: {row: any; index: number}) => (<tr>
+  {Object.values(row).map((data: any, i: number) => (
+    <TableCell key={`${index}${i}`} data={data}/>
+  ))}
+</tr>);
+const HeaderCell = ({text}: {text: string}) => <th>{text}</th>;
+const TableCell = ({data}: {data: any}) => <td> {
+  typeof data === 'object' ?
+  JSON.stringify(data) :
+  data.toString()
+  } </td>;
